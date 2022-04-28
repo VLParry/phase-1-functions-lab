ad1092c9d26c091cae4441a8e0846a623d338a65
Victoria Parry <vlparry@gmail.com>
2022-04-27 21:41:42 -0600

    commit tree b68818bacca6d5d7194ec5e97c0c9977b3af7579

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street1) {
+    return Math.abs(42 - street1)
+}
+
+function distanceFromHqInFeet(street1) {
+    return distanceFromHqInBlocks(street1) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264
+    } else {
+        return (destination - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance < 400) {
+        return 0
+    } else if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400)
+    } else if (distance > 2000 && distance < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+
+}
